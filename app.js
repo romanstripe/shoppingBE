e1aca171d8fd7c583efd02c70e212673ba71dcc5
Sey <ppresi14789@gmail.com>
2025-08-01 23:43:53 +0900

    Create schemas about shopping mall- BE

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //req.body 를 객체로 인식하기 위해 필요
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: false })
+  .then(() => console.log('DB connection is successed!'))
+  .catch((err) => console.log('DB connection is failed', err));
+
+app.listen(process.env.PORT || 5000, () => console.log('Server On!'));
